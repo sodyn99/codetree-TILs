bbf2971499038e82476085120a829432b8a0206b
Sungjin Lee <sodyn99@gmail.com>
2025-01-22 16:53:41 +0900

    Title: 배열의 평균, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250122/배열의 평균/ave-of-array.py
@@ -0,0 +1,8 @@
+a = [list(map(int, input().split())) for i in range(2)]
+
+print(f"{sum(a[0]) / len(a[0]):.1f} {sum(a[1]) / len(a[1]):.1f}")
+
+for i in range(len(a[0])):
+    print(f"{(a[0][i] + a[1][i]) / 2:.1f}", end=" ")
+print()
+print(f"{(sum(a[0]) + sum(a[1])) / (len(a[0]) + len(a[1]))}")
