62a6e7107a83882d7539a2db224bb3b340f5b23c
Sungjin Lee <sodyn99@gmail.com>
2025-01-22 16:54:04 +0900

    Title: 배열의 평균, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250122/배열의 평균/ave-of-array.py
+++ 250122/배열의 평균/ave-of-array.py
@@ -5,4 +5,4 @@ print(f"{sum(a[0]) / len(a[0]):.1f} {sum(a[1]) / len(a[1]):.1f}")
 for i in range(len(a[0])):
     print(f"{(a[0][i] + a[1][i]) / 2:.1f}", end=" ")
 print()
-print(f"{(sum(a[0]) + sum(a[1])) / (len(a[0]) + len(a[1]))}")
+print(f"{(sum(a[0]) + sum(a[1])) / (len(a[0]) + len(a[1])):.1f}")
