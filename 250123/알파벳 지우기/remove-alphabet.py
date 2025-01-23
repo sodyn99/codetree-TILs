ebb53a0b21b068e25bca529437e3b4d3d3c36c1c
Sungjin Lee <sodyn99@gmail.com>
2025-01-23 17:03:13 +0900

    Title: 알파벳 지우기, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250123/알파벳 지우기/remove-alphabet.py
@@ -0,0 +1,14 @@
+s = [input() for _ in range(2)]
+s1, s2 = s
+
+def num(s):
+    s = list(s)
+    S = ""
+    for i in s:
+        if i.isdigit():
+            S += i
+        else:
+            continue
+    return int(S)
+
+print(num(s1) + num(s2))
