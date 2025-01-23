6af7f50352c7d2f510f5078e22a9559bdfe187ec
Sungjin Lee <sodyn99@gmail.com>
2025-01-23 15:27:29 +0900

    Title: 문자열 놀이, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250123/문자열 놀이/play-with-string.py
+++ 250123/문자열 놀이/play-with-string.py
@@ -7,9 +7,7 @@ for i in range(q):
     s = list(s)
     if Q[0] == 1:
         Q[1], Q[2] = int(Q[1])-1, int(Q[2])-1
-        t = s[Q[1]]
-        s[Q[1]] = s[Q[2]]
-        s[Q[2]] = t
+        s[Q[1]], s[Q[2]] = s[Q[2]], s[Q[1]]
         print(''.join(s))
     else:
         for j in range(len(s)):
