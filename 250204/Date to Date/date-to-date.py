7b61d3ff5921fe27f40fce0d295e7d5b1d412e7c
Sungjin Lee <sodyn99@gmail.com>
2025-02-04 11:44:49 +0900

    Title: Date to Date, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250204/Date to Date/date-to-date.py
@@ -0,0 +1,17 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Write your code here!
+
+arr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+d = 1
+while True:
+    if m1 == m2 and d1 == d2:
+        break
+    d1 += 1
+    d += 1
+    if d1 > arr[m1-1]:
+        m1 += 1
+        d1 = 1
+
+print(d)
