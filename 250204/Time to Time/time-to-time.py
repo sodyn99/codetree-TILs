ae597476e062613cbb20d8ce1c82de7cd927de6a
Sungjin Lee <sodyn99@gmail.com>
2025-02-04 11:11:51 +0900

    Title: Time to Time, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250204/Time to Time/time-to-time.py
+++ 250204/Time to Time/time-to-time.py
@@ -2,6 +2,14 @@ a, b, c, d = map(int, input().split())
 
 # Write your code here!
 
-t = (c - a) * 60 + d - b
+t = 0
+while True:
+    if a == c and b == d:
+        break
+    b += 1
+    t += 1
+    if b == 60:
+        a += 1
+        b = 0
 
 print(t)
