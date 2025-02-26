fa31ab27bcfbc1dcf80e76686e3f8538b44e8de6
Sungjin Lee <sodyn99@gmail.com>
2025-02-26 19:59:19 +0900

    Title: Date to Date, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250226/Date to Date/date-to-date.py
+++ 250226/Date to Date/date-to-date.py
@@ -4,13 +4,13 @@ m1, d1, m2, d2 = map(int, input().split())
 
 num_of_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-d = 0
+d = 1
 
 while not (m1 == m2 and d1 == d2):
     d1 += 1
     d += 1
     if d1 > num_of_days[m1-1]:
-        d1 = 0
+        d1 = 1
         m1 += 1
 
 print(d)
