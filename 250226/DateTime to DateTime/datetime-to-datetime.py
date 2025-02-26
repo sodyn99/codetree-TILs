57fdad9452e6a272ec9e175a10ddcab1d61ed4e2
Sungjin Lee <sodyn99@gmail.com>
2025-02-26 22:15:53 +0900

    Title: DateTime to DateTime, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250226/DateTime to DateTime/datetime-to-datetime.py
+++ 250226/DateTime to DateTime/datetime-to-datetime.py
@@ -2,7 +2,11 @@ A, B, C = map(int, input().split())
 
 d, h, m = 11, 11, 11
 t = 0
+
 while not (d == A and h == B and m == C):
+    if A == 11 and (B < h or (B >= h and C < m)):
+        t = -1
+        break
     m += 1
     t += 1
     if m == 60:
