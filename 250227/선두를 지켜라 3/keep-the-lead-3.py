92a6af61cd01b41d22ff1b2bc45c4ca2398af6b9
Sungjin Lee <sodyn99@gmail.com>
2025-02-27 16:20:17 +0900

    Title: 선두를 지켜라 3, Time: 194ms, Memory: 51MB, Status: Passed - CodeTree

---
+++ 250227/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,29 @@
+N, M = map(int, input().split())
+A = [tuple(map(int, input().split())) for _ in range(N)]
+B = [tuple(map(int, input().split())) for _ in range(M)]
+
+p_A, p_B = [0], [0]
+for a in A:
+    for t in range(a[1]):
+        p_A.append(p_A[-1] + a[0])
+for b in B:
+    for t in range(b[1]):
+        p_B.append(p_B[-1] + b[0])
+
+prize = []
+max_d = 0
+for i in range(len(p_A)):
+    tmp = []
+    max_d = max(p_A[i], p_B[i])
+    if p_A[i] == max_d:
+        tmp.append('A')
+    if p_B[i] == max_d:
+        tmp.append('B')
+    prize.append(tmp)
+    
+changed = 0
+for i in range(1, len(prize)):
+    if prize[i] != prize[i-1]:
+        changed += 1
+
+print(changed)
