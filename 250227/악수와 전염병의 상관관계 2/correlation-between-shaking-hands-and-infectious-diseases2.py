fdc269d1f08f5b8a766c3fb57b8e1626f9e5a02f
Sungjin Lee <sodyn99@gmail.com>
2025-02-27 16:07:12 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 250227/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 250227/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,6 +1,6 @@
 N, K, P, T = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(T)]
-arr.sort()
+arr.sort(key = lambda x: x[0])
 
 developer = [0 for _ in range(N)]
 spread = [0 for _ in range(N)]
@@ -8,13 +8,15 @@ developer[P-1] = 1
 
 for i in range(T):
     t, x, y = arr[i][0], arr[i][1] - 1, arr[i][2] - 1
-    if developer[x] == 1:
-        if spread[x] < K:
-            developer[y] = 1
-            spread[x] += 1
-    if developer[y] == 1:
-        if spread[y] < K:
-            developer[x] = 1
-            spread[y] += 1
+    
+    if developer[x]:
+        spread[x] += 1
+    if developer[y]:
+        spread[y] += 1
+
+    if developer[x] and spread[x] <= K:
+        developer[y] = 1
+    if developer[y] and spread[y] <= K:
+        developer[x] = 1
             
 print("".join(list(map(str, developer))))
