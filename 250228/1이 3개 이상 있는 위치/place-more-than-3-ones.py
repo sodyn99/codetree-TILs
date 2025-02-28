95a4d12991c5e4ef05d7c39ed05defe2a7459b6f
Sungjin Lee <sodyn99@gmail.com>
2025-02-28 15:39:33 +0900

    Title: 1이 3개 이상 있는 위치, Time: 75ms, Memory: 18MB, Status: Passed - CodeTree

--- 250228/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 250228/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,8 +1,8 @@
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-dx = [1, 0, -1, 0]
-dy = [0, -1, 0, 1]
+dxs = [1, 0, -1, 0]
+dys = [0, -1, 0, 1]
 
 def is_range(x, y):
     return 0 <= x < N and 0 <= y < N
@@ -12,9 +12,9 @@ cnt = 0
 for i in range(N):
     for j in range(N):
         tmp = 0
-        for k in range(4):
-            x = j + dx[k]
-            y = i + dy[k]
+        for dx, dy in zip(dxs, dys):
+            x = j + dx
+            y = i + dy
             if is_range(x, y) and arr[y][x] == 1:
                 tmp += 1
         if tmp >= 3:
