6c363a45664a646ec9a7aa821ae0baffb9791ee8
Sungjin Lee <sodyn99@gmail.com>
2025-02-28 19:23:28 +0900

    Title: 방향에 맞춰 이동, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250228/방향에 맞춰 이동/move-in-direction.py
+++ 250228/방향에 맞춰 이동/move-in-direction.py
@@ -1,16 +1,10 @@
 N = int(input())
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
-D = ['E', 'W', 'S', 'N']
-
+direction = {'W': (-1, 0), 'S': (0, -1), 'N': (0, 1), 'E': (1, 0)}
 x, y = 0, 0
 for _ in range(N):
-    l, d = input().split()
-    d = int(d)
-    index = D.index(l)
-
-    x += d * dx[index]
-    y += d * dy[index]
+    d, l = input().split()
+    x += direction[d][0] * int(l)
+    y += direction[d][1] * int(l)
 
 print(x, y)
