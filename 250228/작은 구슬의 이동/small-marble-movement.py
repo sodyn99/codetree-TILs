d09f3c617eb528182b5903faa38d154974dbbd83
Sungjin Lee <sodyn99@gmail.com>
2025-02-28 23:15:31 +0900

    Title: 작은 구슬의 이동, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250228/작은 구슬의 이동/small-marble-movement.py
+++ 250228/작은 구슬의 이동/small-marble-movement.py
@@ -4,21 +4,19 @@ R, C, D = input().split()
 arr = [[0 for _ in range(N)] for _ in range(N)]
 d_num = {'R': 0, 'D': 1, 'U': 2, 'L': 3}
 d_num = d_num[D]
-dx, dy = [1, 0, 0, -1], [0, -1, 1, 0]
+dx, dy = [1, 0, 0, -1], [0, 1, -1, 0]
 
 def is_range(x, y):
-    return 0 <= x < N and 0 <= y < N
+    return 0 < x <= N and 0 < y <= N
 
-x, y = int(C) - 1, int(R) - 1
+x, y = int(C), int(R)
 t = 0
-while t < T:
-    if not is_range(x, y):
+for _ in range(T):
+    nx = x + dx[d_num]
+    ny = y + dy[d_num]
+    if not is_range(nx, ny):
         d_num = 3 - d_num
-        x += dx[d_num]
-        y += dy[d_num]
     else:
-        t += 1
-        x += dx[d_num]
-        y += dy[d_num]
+        x, y = nx, ny
     
-print(y + 1, x + 1)
+print(y, x)
