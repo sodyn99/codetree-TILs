20f45d98688cd7ce65629d7e3716414e6de0163b
Sungjin Lee <sodyn99@gmail.com>
2025-03-09 01:33:34 +0900

    Title: 데이터센터의 온도 조정 2, Time: 85ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250309/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,22 @@
+N, C, G, H = map(int, input().split())
+Temp = [tuple(map(int, input().split())) for _ in range(N)]
+
+def work_by_machine(t, Ta, Tb):
+    work = 0
+    if t < Ta:
+        work = C
+    elif Ta <= t < Tb:
+        work = G
+    else:
+        work = H
+    return work
+    
+
+max_work = 0
+for t in range(1000):
+    max_work_by_machine = 0
+    for T in Temp:
+        max_work_by_machine += work_by_machine(t, T[0], T[1])
+    max_work = max(max_work, max_work_by_machine)
+
+print(max_work)
