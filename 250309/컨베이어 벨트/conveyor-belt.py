d52821bf4156368a7b1816bcb3b0625e69543df2
Sungjin Lee <sodyn99@gmail.com>
2025-03-09 23:04:52 +0900

    Title: 컨베이어 벨트, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250309/컨베이어 벨트/conveyor-belt.py
+++ 250309/컨베이어 벨트/conveyor-belt.py
@@ -1,15 +1,7 @@
 N, T = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(2)]
-for _ in range(T):
-    temp1 = arr[0][-1]
-    temp2 = arr[1][-1]
-    for i in range(N-1, 0, -1):
-        arr[0][i] = arr[0][i-1]
-        arr[1][i] = arr[1][i-1]
-    arr[1][0] = temp1
-    arr[0][0] = temp2
+arr = list(map(int, input().split())) + list(map(int, input().split()))
 
-for a in arr:
-    print(*a)
+arr = arr[2*N-(T%(2*N)):] + arr[:2*N-(T%(2*N))]
 
-
+print(" ".join(map(str, arr[:N])))
+print(" ".join(map(str, arr[N:])))
