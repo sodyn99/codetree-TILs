6de60c6e827c91fa39c91519078d4164e05d3a31
Sungjin Lee <sodyn99@gmail.com>
2025-03-20 14:13:11 +0900

    Title: 삼각형 컨베이어 벨트, Time: 54ms, Memory: 16MB, Status: Passed - CodeTree

--- 250320/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
+++ 250320/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -3,9 +3,9 @@ arr = [list(map(int, input().split())) for _ in range(3)]
 
 for t in range(T):
     tmp1, tmp2, tmp3 = [arr[0][-1]], [arr[1][-1]], [arr[2][-1]]
-    arr[0] = tmp3 + arr[0][:2]
-    arr[1] = tmp1 + arr[1][:2]
-    arr[2] = tmp2 + arr[2][:2]
+    arr[0] = tmp3 + arr[0][:N-1]
+    arr[1] = tmp1 + arr[1][:N-1]
+    arr[2] = tmp2 + arr[2][:N-1]
 
 for a in arr:
     print(*a)
