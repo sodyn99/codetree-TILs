c300190a9c73f24c141a352fee21d01b738a73fa
Sungjin Lee <sodyn99@gmail.com>
2025-03-21 14:49:39 +0900

    Title: 최단 Run Length 인코딩, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250321/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 250321/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -1,19 +1,17 @@
 A = list(input())
-m = len(A)
+m = len(A) * 2
 for a in range(len(A)):
-    tm = 0
     tmp_arr = []
     tmp = 1
     for t in range(1, len(A)):
         if A[t] == A[t-1]:
             tmp += 1
         else:
+            # tmp_arr.append(len(str(tmp)))
             tmp_arr.append(len(str(tmp)))
-            tm += 1
             tmp = 1
-    tm += 1
     tmp_arr.append(len(str(tmp)))
-    m = min(m, tm + sum(tmp_arr))
+    m = min(m, len(tmp_arr) + sum(tmp_arr))
     A = [A[-1]] + A[:len(A)-1]
 
 print(m)
