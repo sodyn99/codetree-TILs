2c6bfa49c90958cf9a927747a245f966c09c84c8
Sungjin Lee <sodyn99@gmail.com>
2025-04-01 18:57:18 +0900

    Title: 1차원 폭발 게임, Time: 52ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250401/1차원 폭발 게임/The-1D-bomb-game.py
+++ 250401/1차원 폭발 게임/The-1D-bomb-game.py
@@ -1,23 +1,31 @@
 N, M = map(int, input().split())
 bombs = [int(input()) for _ in range(N)]
 
+def explosion(bombs, i, t):
+    if t >= M:
+        if i + 1 < len(bombs):
+            if bombs[i+1] != bombs[i]:
+                for j in range(i-t+1, i+1):
+                    bombs[j] = 0
+                t = 1
+        elif i == len(bombs) - 1:
+            for j in range(i-t+1, i+1):
+                bombs[j] = 0
+            t = 1
+    return bombs, t
+
+
 n = 0
 while True:
     t = 1
     tmp_arr = []
-    for i in range(1, len(bombs)):
-        if bombs[i] == bombs[i-1]:
-            t += 1
-            if t >= M:
-                if i + 1 < len(bombs):
-                    if bombs[i+1] != bombs[i]:
-                        for j in range(i-t+1, i+1):
-                            bombs[j] = 0
-                        t = 1
-                elif i == len(bombs) - 1:
-                    for j in range(i-t+1, i+1):
-                        bombs[j] = 0
-                    t = 1
+    for i in range(len(bombs)):
+        if i-1 >= 0:
+            if bombs[i] == bombs[i-1]:
+                t += 1
+                bombs, t = explosion(bombs, i, t)
+        else:
+            bombs, t = explosion(bombs, i, t)
     for b in bombs:
         if b != 0:
             tmp_arr.append(b)
