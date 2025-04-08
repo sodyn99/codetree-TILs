6c2ce23dbcae25201e6640915962a55208c73d2c
Sungjin Lee <sodyn99@gmail.com>
2025-04-08 21:27:51 +0900

    Title: 마을 구분하기, Time: 67ms, Memory: 18MB, Status: Passed - Codetree

--- 250408/마을 구분하기/seperate-village.py
+++ 250408/마을 구분하기/seperate-village.py
@@ -5,14 +5,23 @@ visited = [[0 for _ in range(N)] for _ in range(N)]
 def is_range(x, y):
     return 0 <= x < N and 0 <= y < N
 
+def can_go(x, y):
+    if not is_range(x, y):
+        return False
+    if visited[x][y] == 1:
+        return False
+    if field[x][y] == 0:
+        return False
+    return True
+
 def dfs(x, y):
     global cnt
+    visited[x][y] = 1
+    cnt += 1
     dxs, dys = [1, 0, -1, 0], [0, 1, 0, -1]
     for dx, dy in zip(dxs, dys):
         nx, ny = x + dx, y + dy
-        if is_range(nx, ny) and visited[nx][ny] == 0 and field[nx][ny] == 1:
-            visited[nx][ny] = 1
-            cnt += 1
+        if can_go(nx, ny):
             dfs(nx, ny)
     return cnt
 
@@ -20,7 +29,7 @@ villages = []
 for i in range(N):
     for j in range(N):
         cnt = 0
-        if visited[i][j] == 0:
+        if can_go(i, j):
             villages.append(dfs(i, j))
 
 new_villages = []
