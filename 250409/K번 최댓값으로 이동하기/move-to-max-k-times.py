860640037df362326f120b9440dd34be366b778f
Sungjin Lee <sodyn99@gmail.com>
2025-04-09 23:46:29 +0900

    Title: K번 최댓값으로 이동하기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250409/K번 최댓값으로 이동하기/move-to-max-k-times.py
+++ 250409/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -6,59 +6,52 @@ def remap(i):
     return int(i) - 1
 start = tuple(map(remap, input().split()))
 visited = [[0 for _ in range(N)] for _ in range(N)]
-no = [[0 for _ in range(N)] for _ in range(N)]
+q = deque()
 
 def is_range(x, y):
     return 0 <= x < N and 0 <= y < N
 
-def can_go(x, y):
+def can_go(x, y, target):
     if not is_range(x, y):
         return False
-    if visited[x][y] or no[x][y]:
+    if visited[x][y] or arr[x][y] >= target:
         return False
     return True
 
 def bfs():
     global start
-    q = deque([start])
+    q.append(start)
     max_num = 0
-    tmp = []
     while q:
         dxs = [-1, 0, 1, 0]
         dys = [ 0, 1, 0,-1]
         x, y = q.popleft()
-        for i in range(N):
-            for j in range(N):
-                if arr[i][j] >= arr[start[0]][start[1]]:
-                    no[i][j] = 1
         for dx, dy in zip(dxs, dys):
             nx, ny = x + dx, y + dy
-            if can_go(nx, ny):
+            if can_go(nx, ny, arr[start[0]][start[1]]):
                 visited[nx][ny] = 1
-                q.append((nx, ny))
-                tmp.append((nx, ny))
+                if (nx, ny) not in q:
+                    q.append((nx, ny))
                 if arr[nx][ny] > max_num and (nx, ny) != start:
                     max_num = arr[nx][ny]
+    return max_num
 
-    (next_x, next_y) = (N-1, N-1)
-    for tx, ty in tmp:
-        if arr[tx][ty] == max_num:
-            (next_x, next_y) = min((next_x, next_y), (tx, ty), key=lambda x: (x[0], x[1]))
-            # print(max_num, (tx, ty), (next_x, next_y))
-    start = (next_x, next_y)
-
-    return next_x, next_y
-
-X, Y = 0, 0
 for _ in range(K):
     for i in range(N):
         for j in range(N):
             visited[i][j] = 0
     
-    newX, newY = bfs()
-    if (newX, newY) == (X, Y):
+    max_num = bfs()
+    
+    next_x, next_y = N-1, N-1
+    for i in range(N):
+        for j in range(N):
+            if visited[i][j] and arr[i][j] == max_num:
+                (next_x, next_y) = min((next_x, next_y), (i, j), key=lambda x: (x[0], x[1]))
+    
+    if max_num == 0:
         break
-    X, Y = newX, newY
+    start = (next_x, next_y)
 
-print(X++1, Y+1)
+print(start[0]+1, start[1]+1)
                 
